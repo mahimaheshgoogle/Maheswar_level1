385648fa622d2a1aba75f64d5cd92138d823098f
mahimaheshgoogle <75848880+mahimaheshgoogle@users.noreply.github.com>
2021-02-25 07:03:26 -0500

    remember commit

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,60 @@
 //WAP to find the sum of n fractions.
+#include<stdio.h>
+typedef struct
+{
+int nume;
+int deno;
+}fraction;
+int range()
+{
+int n;
+printf("Enter the number of fractions: ");
+scanf("%d",&n);
+return n;
+}
+int input(int n,fraction a[n])
+{
+for(int i = 0;i<n;i++)
+ {
+   printf("Enter the  %d numerator and denominator :",i+1);
+   scanf("%d%d",&[i].nume,&a[i].deno);
+   }
+}
+fraction sum(int n,fraction a[n])
+{
+int numerator = 0, denominator = 1;
+for(int i=0;i<n;i++)
+{
+denominator* = a[i].deno;
+}
+for (int i=0;i<n;i++)
+{
+numerator +=(a[i].nume)*(denominator/a[i].deno);
+}
+fraction add={numerator,denominator];
+int gcd=1;
+for (int i=1;i<=add.nume&&i<=add.deno;i++)
+{
+  if(add.nume%i==0&&add.deno%i==0)
+{
+gcd=i;
+}
+}
+add.nume=add.num/gcd;
+add.den0=add.deno/gcd;
+return add;
+}
+void display(intb n,fraction add)
+{
+printf("the sum of %d fractions entered is %d/%d",n,add.nume,add.deno);
+}
+int main()
+{
+int n;
+n=range();
+fraction a[n];
+input(n,a);
+fraction result=sum(n,a);
+display(n,result);
+return 0;
+}
